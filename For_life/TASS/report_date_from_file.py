0fbef4dbd6ed08b9ceb062fa25173eeae1d1f404
evgeniy <pavlinb4@gmail.com>
2021-12-15 10:53:13 +0300

    устаревший модуль модуль для отчета из ТАСС

---
+++ For_life/TASS/report_date_from_file.py
@@ -0,0 +1,9 @@
+# из отчета TASS извлекает период отчета для
+import openpyxl
+
+
+def get_report_date(file_way):
+    wb = openpyxl.load_workbook(file_way)
+    sheet = wb.active
+    report_date = sheet.cell(row=5, column=3).value
+    return report_date
