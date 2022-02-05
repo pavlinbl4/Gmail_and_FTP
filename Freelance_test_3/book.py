e30b999e49e23e746ec04e8aae3198139352efef
pavlinbl4 <pavlinbl4@gmail.com>
2022-02-05 23:10:49 +0300

    с помощю селениума получилось дойти до страницы с информацией, которую нужно парсить

--- Freelance_test_3/book.py
+++ Freelance_test_3/book.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.webdriver.common.keys import Keys
 import time
@@ -8,11 +9,31 @@ driver = webdriver.Firefox()
 
 site = "http://1kas.sudrf.ru/modules.php?name=sud_delo#"
 driver.get(site)
-driver.find_element(By.ID,"top_menu").find_element(By.TAG_NAME,"b").click()
-time.sleep(5)
-driver.find_element(By.NAME,"dic").click()
-time.sleep(15)
+driver.find_element(By.ID, "top_menu").find_element(By.TAG_NAME, "b").click()
+time.sleep(1)
+driver.find_element(By.NAME, "dic").click()
+
+driver.find_element(By.ID, 'popup')
+driver.find_element(By.CLASS_NAME, "bsrLawBook").find_element(By.NAME, "lwbart-sublevel").click()
+
+# driver.find_element(By.ID,"lwbart-inp-560001").click()
 
 
+check_box = driver.find_element(By.ID, "lwbart-inp-780000")
+actions = ActionChains(driver)
+actions.move_to_element(check_box)
+driver.find_element(By.ID, "lwbart-inp-780000").click()
+driver.find_element(By.CSS_SELECTOR, '#sublevel-00 > ul:nth-child(1) > li:nth-child(17) > a:nth-child(1)').click()
+
+driver.find_element(By.ID, "lwbart-inp-780010").click()
+driver.find_element(By.ID, "lwbart-inp-780020").click()
+driver.find_element(By.ID, "lwbart-inp-780030").click()
+driver.find_element(By.ID, "lwbart-inp-780040").click()
+
+driver.find_element(By.ID, "cat_close").find_element(By.TAG_NAME, "a").click()
+time.sleep(20)
+
+driver.find_element(By.NAME, "Submit").click()
+time.sleep(20)
 
 driver.close()
