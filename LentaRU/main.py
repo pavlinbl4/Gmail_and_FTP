fbce1ad9bc17ed217d326aca50c0a2b12c4b759b
pavlinbl4 <pavlinbl4@gmail.com>
2022-02-08 16:06:02 +0300

    даже с задержкой в 1 секунду вылеает с ошибкой

--- LentaRU/main.py
+++ LentaRU/main.py
@@ -1,16 +1,19 @@
 import requests
 import json
+import csv
+import time
 
 
-# def csv_writer(i):
-#     with open("lenta_ru.csv", 'a') as input_file:
-#         columns = ['title', 'image_url', 'url', "pubdate"]
-#         writer = csv.DictWriter(input_file, fieldnames=columns)
-#         writer.writerow(i)
+def csv_writer(i):
+    with open("lenta_ru.csv", 'a') as input_file:
+        columns = ['title', 'image_url', 'url', "pubdate"]
+        writer = csv.DictWriter(input_file, fieldnames=columns)
+        writer.writerow(i)
 
 
 def get_html(url):
     r = requests.get(url)
+    time.sleep(1)
     return r.text
 
 
@@ -18,21 +21,19 @@ def main():
     number = 0
 
     while True:
-        url = f"https://lenta.ru/search/v2/process?from={number}&size=10&sort=2&title_only=0&domain=1&modified,format=yyyy-MM-dd&query=Фото: Евгений Павленко / «Коммерсантъ»"
+        url = f"https://lenta.ru/search/v2/process?from={str(number)}&size=10&sort=2&title_only=0&domain=1&modified,format=yyyy-MM-dd&query=Фото: Евгений Павленко / «Коммерсантъ»"
         rezult = json.loads(get_html(url))
+
         number += 1
         print(f"page number: {number}, записи в файле {len(rezult['matches'])}")
         if len(rezult['matches']) == 0:
+            print('empty json file')
             break
 
-
-        with open(f"data_file_{number}.json", "a") as write_file:
-            json.dump(rezult, write_file)
-        # for i in rezult["matches"]:
-        #     print(i['title'])
-        #     print(i['image_url'])
-        #     print(i['url'])
-        #     print(i["pubdate"])
+        for i in rezult['matches']:
+            main_data = {'title': i['title'], 'image_url': i['image_url'], 'url': i['url'], 'pubdate': time.ctime(i['pubdate'])}
+            print(main_data)
+            csv_writer(main_data)
 
 
 if __name__ == '__main__':
