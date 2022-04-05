719ba39a8fed4961acefa0bf3736bc576446f534
evgeniy <pavlinb4@gmail.com>
2022-04-05 21:10:50 +0300

    скрипт проверяет появились ли новые снимки в агенстве, если да, то создает списко всех изображений и сохраняет их в xlsx  файле, после этого сравнивает с предыдущими данными и скачивает превью добавленных снимков

---
+++ TASS_after_update/check_my_uploads.py
@@ -0,0 +1,108 @@
+"""проверяю, добавили ли мои фото на сайт, скрипт отрабатывает по crontab и
+ заносит информацию в таблицу, если количество снимков изменилось, то запускаю скрипт
+ all_images_new и потом добавляет свежие снимки в заданную папку """
+
+from openpyxl import load_workbook
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pandas as pd
+import datacompy
+import os
+import requests
+import re
+import subprocess
+import sys
+
+options = webdriver.ChromeOptions()
+options.headless = True  # фоновый режим
+options.add_argument(
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4200.0 Iron Safari/537.36")
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+browser = webdriver.Chrome(options=options)
+
+
+def new_pictures_links(last_date, previous_date):
+    pd.options.display.max_colwidth = 100
+    photo_base = '/Volumes/big4photo/Documents/TASS/Tass_data/all_TASS_images.xlsx'
+    last_df = pd.read_excel(photo_base, sheet_name=last_date)
+    previos_df = pd.read_excel(photo_base, sheet_name=previous_date)
+    difference = datacompy.Compare(previos_df, last_df,
+                                   join_columns=['image_id', 'image_date', 'image_caption']).df2_unq_rows
+
+    return difference
+
+
+def image_downloader(difference, last_date):
+    folder = '/Volumes/big4photo/Documents/TASS/Tass_data/added_images'
+    os.makedirs(f"{folder}/{last_date}", exist_ok=True)
+    for i in range(len(difference)):
+        image_url = difference.image_link.iloc[i]
+        r = requests.get(image_url)
+        image_name = re.findall(r'\d+(?=\.thw)', image_url)[0]
+        print(image_name)
+
+        with open(f"{folder}/{last_date}/{image_name}.jpg", 'wb') as download_file:
+            for chunk in r.iter_content(9000):
+                download_file.write(chunk)
+
+
+def get_html(link):
+    browser.get('https://www.tassphoto.com/ru')
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "userrequest"))
+    )
+    search_input = browser.find_element(By.ID, "userrequest")
+    search_input.clear()
+    search_input.send_keys('Семен Лиходеев')
+    search_input.send_keys(Keys.ENTER)
+    html = browser.page_source
+    browser.close()
+    browser.quit()
+    return html
+
+
+def parser():
+    link = 'https://www.tassphoto.com/ru'
+    html = get_html(link)
+    soup = BeautifulSoup(html, 'lxml')
+    return str(soup.select(".result-counter#nb-result"))[42:47]
+
+
+def add_data(photos_count):
+    file = "/Volumes/big4photo/Documents/TASS/Tass_data/TASS_photos.xlsx"
+    book = load_workbook(file)
+    ws = book.active
+    last_row = ws.max_row
+
+    old_value = ws.cell(row=last_row, column=2).value  # последние данные в таблице
+
+    last_date = f'{ws.cell(row=last_row, column=1).value[:10]}'
+    previous_date = f'{ws.cell(row=(last_row - 1), column=1).value[:10]}'
+
+    if ws.cell(row=last_row, column=2).value != photos_count:
+        ws.cell(row=last_row + 1, column=2).value = photos_count
+        ws.cell(row=last_row + 1, column=1).value = datetime.now().strftime('%Y-%m-%d %H:%M')
+        new_images = int(photos_count) - int(old_value)
+        ws.cell(row=last_row + 1, column=3).value = new_images
+        print(f'добавлено {new_images} снимков')
+        subprocess.run([sys.executable, 'all_images_new.py'])
+        difference = new_pictures_links(last_date, previous_date)
+        image_downloader(difference, last_date)
+
+    else:
+        print(f"no new images, now  {photos_count} - images online")
+
+    book.save(file)
+    book.close()
+
+    # difference = new_pictures_links(last_date, previous_date)
+    # image_downloader(difference, last_date)
+
+
+add_data(parser())
